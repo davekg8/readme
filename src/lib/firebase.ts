68a297fca4037d32145bf2a37ebbecc345b41801
David Egbakou <egbdavid@gmail.com>
2025-07-14 18:20:05 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -14,10 +14,10 @@ const firebaseConfig = {
 };
 
 
-const serviceAccount: ServiceAccount = {
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+const serviceAccount = {
+    project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    private_key: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
 }
 
 
